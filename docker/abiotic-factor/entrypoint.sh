955d729e1fe7080513ddf52a2fb4a38caf20e0d6
KianSajjadi <kian.s.sajjadi@gmail.com>
2026-03-08 20:41:37 +1100

    update pull policy

--- docker/abiotic-factor/entrypoint.sh
+++ docker/abiotic-factor/entrypoint.sh
@@ -17,17 +17,21 @@ WorldSaveName="${WorldSaveName:-Cascade}"
 AdditionalArgs="${AdditionalArgs:-}"
 
 # Check for updates/perform initial installation
-if [ ! -d "/server/AbioticFactor/Binaries/Win64" ] || [[ $AutoUpdate == "true" ]]; then
+if [ ! -f "/server-data/AbioticFactorServer.exe" ] || [[ $AutoUpdate == "true" ]]; then
     steamcmd \
     +@sSteamCmdForcePlatformType windows \
-    +force_install_dir /server \
+    +force_install_dir /server-data \
     +login anonymous \
     +app_update 2857200 validate \
     +quit
 fi
 
-pushd /server/AbioticFactor/Binaries/Win64 > /dev/null
-wine AbioticFactorServer-Win64-Shipping.exe $SetUsePerfThreads$SetNoAsyncLoadingThread-MaxServerPlayers=$MaxServerPlayers \
+cd /server-data
+
+# Start Xvfb for headless Wine
+Xvfb :99 -screen 0 1024x768x16 &
+export DISPLAY=:99
+
+wine AbioticFactorServer.exe $SetUsePerfThreads$SetNoAsyncLoadingThread-MaxServerPlayers=$MaxServerPlayers \
     -PORT=$Port -QueryPort=$QueryPort -ServerPassword=$ServerPassword \
     -SteamServerName="$SteamServerName" -WorldSaveName="$WorldSaveName" -tcp $AdditionalArgs
-popd > /dev/null
